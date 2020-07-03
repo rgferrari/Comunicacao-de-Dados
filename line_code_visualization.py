e789ad6453acf3b0694053c86955e046cf625ff4
rgferrari <renegferrari@gmail.com>
2020-07-03 18:39:31 -0300

    Added nrz-l function

--- line_code_visualization.py
+++ line_code_visualization.py
@@ -6,29 +6,35 @@ def visualize_line_code(line_code_name, x_axis, y_axis):
     plt.title(line_code_name)
     plt.show()
 
-# Fiz so pra testar o matplotlib
-def nrz():
-    x_axis = np.arange(0, len(bit_sequence))
-    y_axis = bit_sequence
-    plt.step(x_axis, y_axis)
-    plt.title("NRZ-I")
-    plt.show()
-
 def nrz_i():
     signal = 1
-    x_axis = np.arange(0, len(bit_sequence) + 1)
     y_axis = [signal]
     for bit in bit_sequence:
         if bit == 1:
             signal = -signal
             y_axis.append(signal)
-        if bit == 0:
+        elif bit == 0:
             y_axis.append(signal)
+
+    x_axis = np.arange(0, len(y_axis))
     
     visualize_line_code("NRZ-I", x_axis, y_axis)
 
 def nrz_l():
-    pass
+    signal = 1
+    y_axis = [signal]
+    for bit in bit_sequence:
+        if bit == 1:
+            signal = -1
+            y_axis.append(signal)
+        elif bit == 0:
+            signal = 1
+            y_axis.append(signal)
+
+    x_axis = np.arange(0, len(y_axis))
+
+    visualize_line_code("NRZ-L", x_axis, y_axis)
+
 def pseudoternary():
     pass
 def manchester():
@@ -42,17 +48,18 @@ if __name__ == "__main__":
         bit_sequence = [int(i) for i in input().split()]
 
         option = int(input("0 - Quit \n1 - NRZ-I \n2 - NRZ-L \n3 - Pseudoternary\n4 - Manchester\n5 - Differential Manchester\n"))
+
         if option == 0:
             quit()
         elif option == 1:
             nrz_i()
         elif option == 2:
-            pass
+            nrz_l()
         elif option == 3:
-            pass
+            pseudoternary()
         elif option == 4:
-            pass
+            manchester()
         elif option == 5:
-            pass
+            differential_manchester()
         else:
             print("Choose a valid option!\n")
